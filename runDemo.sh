00d2323e28310fe05efcd918ea2169636ad01a88
zh460045050 <32474206+zh460045050@users.noreply.github.com>
2021-04-13 10:58:53 +0800

    Update runDemo.sh

--- runDemo.sh
+++ runDemo.sh
@@ -1,4 +1,4 @@
-python libs/demo.py --n_spix 100 \
+python demo.py --n_spix 100 \
               --img_path "./demo_imgs/107793.jpg" \
               --check_path "./lnsnet_BSDS_checkpoint.pth" \
               --seed_strategy 'network'
